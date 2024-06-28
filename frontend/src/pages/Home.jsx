731c742f99b4d13155684dcebdb13d23b56ec1d5
Aman Shrestha <amanshrestha147181@gmail.com>
2024-06-28 14:02:27 +0545

    asdsndkasdasdasdasdasdasdasdasd

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Testimonial from "../components/Testimonial";
 import FAQ from "../components/FAQ";
 import Quote from "../components/Quote";
 import HomePackages from "../components/HomePackages";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -12,12 +13,14 @@ const Home = () => {
           Let’s enjoy your desired trip with{" "}
           <span className="font-play text-4xl font-bold italic text-[#F64F02]">Travello</span>
         </h1>
-        <h1 className="text-xl font-roboto text-gray-400">
+        <h1 className="text-xl font-roboto px-2 text-center text-gray-400">
         A multinational Travel Management Company with a 25-year long global presence and expertise in business travel sector.
         </h1>
+        <Link to="/packages">
         <button className="rounded-full bg-[#F64F02] hover:bg-[#F64F02] text-white px-7 py-2">
           View Packages
         </button>
+         </Link> 
       </div>
       <HomePackages/>
       <div className="px-4 sm:px-12 md:px-20 lg:px-28 xl:px-32 ">
